6e948e8abdd370e6b2d5a82005301fd38e1f1e4e
TMaskpl <75216446+TMaskpl@users.noreply.github.com>
2020-11-30 23:17:17 -0800

    Update tmask.ps1

--- tmask.ps1
+++ tmask.ps1
@@ -15,15 +15,23 @@ Set all remote hosts to trusted. Note: You may want to unset this later.
 Set-Item WSMan:localhost\client\trustedhosts -value *
 
 Get-Item WSMan:\localhost\Client\TrustedHosts
-
+Write-Host "Set password tmask user"
 $Password = Read-Host -AsSecureString
+$y = Get-LocalUser | Where-Object {$_.name -like "tmask"}
+if ( $y )
+{
+Write-Host $y
+}
+else
+{
+New-LocalUser "tmask" -Password $Password -FullName "Service TMask User" -Description "Service User TMask.pl mailto: biuro@tmask.pl"
+}
 
-New-LocalUser "tmask" -Password $Password -FullName "Service TMask User" -Description "Service User TMask.pl"
-
-$x = Get-WinSystemLocale | Where-Object {$_.name -like "en-EN"}
+$x = Get-WinSystemLocale | Where-Object {$_.name -like "en-US"}
 if ( $x ) {
     Add-LocalGroupMember -Group "Administrators" -Member "tmask"
 }
 else {
     Add-LocalGroupMember -Group "Administratorzy" -Member "tmask"
 }
+
